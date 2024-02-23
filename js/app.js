a429b48d1370287b40914b99d2fe5164a11b2f37
Your Name <you@example.com>
2024-02-23 17:28:42 +0330

    version-1

--- js/app.js
+++ js/app.js
@@ -1,37 +1,19 @@
+let firstNumber = Number(prompt("input first number"));
 
-let firstNumber = Number(prompt("input first number"))
+let operator = prompt("what do you want to do");
 
-let operator = prompt("what do you want to do")
+let secondNumber = Number(prompt("input second number"));
 
-let secondNumber = Number(prompt("input second number"))
-
-let operator2 = prompt("what do you want to do")
-
-let thirdNumber = Number(prompt("input third number"))
-
-let result = Number()
-
-if(operator == '+')
-{
-    alert(firstNumber + secondNumber)
-}
-
-else if(operator == '-')
-{
-    alert( firstNumber - secondNumber )
-}
-
-else if(operator == '/')
-{
-    alert( firstNumber / secondNumber )
-}
-
-else if(operator == '*')
-{
-    alert( firstNumber * secondNumber )
-}
-
-if(operator2 == '+')
-{
-    alert( )
+if (operator == "+") {
+  alert(firstNumber + secondNumber);
+} else if (operator == "-") {
+  alert(firstNumber - secondNumber);
+} else if (operator == "/") {
+  alert(firstNumber / secondNumber);
+} else if (operator == "*") {
+  alert(firstNumber * secondNumber);
+} else if (operator == "**") {
+  alert(firstNumber ** secondNumber);
+} else {
+  alert("please input one of the operator");
 }
