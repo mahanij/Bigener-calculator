4389e4a170ec9ebf34869c49697fbdcc048b283c
Your Name <you@example.com>
2024-02-24 17:02:53 +0330

    eror solved

--- js/app.js
+++ js/app.js
@@ -9,51 +9,51 @@ let trueFalse = confirm("do you want to continue");
 if (trueFalse == true) {
   let operations2 = prompt("what do you want to do");
   let thirdNumber = Number(prompt("input third number"));
-  if ((operations == "+", operations2 == "+")) {
+  if ((operations == "+"& operations2 == "+")) {
     alert(firstNumber + secondNumber + thirdNumber);
-  } else if ((operations == "-", operations2 == "-")) {
+  } else if ((operations == "-"& operations2 == "-")) {
     alert(firstNumber - secondNumber - thirdNumber);
-  } else if ((operations == "*", operations2 == "*")) {
+  } else if ((operations == "*"& operations2 == "*")) {
     alert(firstNumber * secondNumber * thirdNumber);
-  } else if ((operations == "/", operations2 == "/")) {
+  } else if ((operations == "/"& operations2 == "/")) {
     alert(firstNumber / secondNumber / thirdNumber);
-  } else if ((operations == "+", operations2 == "-")) {
+  } else if ((operations == "+"& operations2 == "-")) {
     alert(firstNumber + secondNumber - thirdNumber);
-  } else if ((operations == "-", operations2 == "+")) {
+  } else if ((operations == "-"& operations2 == "+")) {
     alert(firstNumber - secondNumber + thirdNumber);
-  } else if ((operations == "*", operations2 == "-")) {
+  } else if ((operations == "*"& operations2 == "-")) {
     alert(firstNumber * secondNumber - thirdNumber);
-  } else if ((operations == "-", operations2 == "*")) {
+  } else if ((operations == "-"& operations2 == "*")) {
     alert(secondNumber * thirdNumber - firstNumber);
-  } else if ((operations == "/", operations2 == "-")) {
+  } else if ((operations == "/"& operations2 == "-")) {
     alert(firstNumber / secondNumber - thirdNumber);
-  } else if ((operations == "-", operations2 == "/")) {
+  } else if ((operations == "-"& operations2 == "/")) {
     alert(secondNumber / thirdNumber - firstNumber);
-  } else if ((operations == "+", operations2 == "*")) {
+  } else if ((operations == "+"& operations2 == "*")) {
     alert(thirdNumber * firstNumber + secondNumber);
-  } else if ((operations == "*", operations2 == "+")) {
+  } else if ((operations == "*"& operations2 == "+")) {
     alert(firstNumber * secondNumber + thirdNumber);
-  } else if ((operations == "+", operations2 == "/")) {
+  } else if ((operations == "+"& operations2 == "/")) {
     alert(thirdNumber / firstNumber + secondNumber);
-  } else if ((operations == "/", operations2 == "+")) {
+  } else if ((operations == "/"& operations2 == "+")) {
     alert(firstNumber / secondNumber + thirdNumber);
-  } else if ((operations == "*", operations2 == "/")) {
+  } else if ((operations == "*"& operations2 == "/")) {
     alert((firstNumber * secondNumber) / thirdNumber);
-  } else if ((operations == "/", operations2 == "*")) {
+  } else if ((operations == "/"& operations2 == "*")) {
     alert((firstNumber / secondNumber) * thirdNumber);
   } else {
     alert("please input on of the operations");
   }
 }
-if (trueFalse == false , operations == "+") {
+if (trueFalse == false & operations == "+") {
   alert(firstNumber + secondNumber);
-} else if (trueFalse == false , operations == "-") {
+} else if (trueFalse == false & operations == "-") {
   alert(firstNumber - secondNumber);
-} else if (trueFalse == false ,operations == "/") {
+} else if (trueFalse == false &operations == "/") {
   alert(firstNumber / secondNumber);
-} else if (trueFalse == false ,operations == "*") {
-  alert(trueFalse == false ,firstNumber * secondNumber);
-} else if (trueFalse == false , operations == "**") {
+} else if (trueFalse == false &operations == "*") {
+  alert(trueFalse == false &firstNumber * secondNumber);
+} else if (trueFalse == false & operations == "**") {
   alert(firstNumber ** secondNumber);
 } else if(trueFalse == false){
   alert("please input operations")
